622a04235c1ef2d43a907f61c2b546271fd2e3b6
Megan Sampson <msampson@nyc.yearup.org>
2022-10-28 20:16:39 -0400

    started first template of the javascript

---
+++ scripts/estimatecost.js
@@ -0,0 +1,30 @@
+"use strict";
+
+window.onload = function() {
+    let Estimatebtn = document.getElementById("Estimatebtn");
+    Estimatebtn.onclick = EstimatebtnOnClick;
+    // other events handlers connected here
+   }
+
+// Get month
+   function getRoomRate(){
+    const checkInDate = document.getElementById("checkInDate");
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September","October", "November", "December"];
+    let inSeasonDate = new Date(checkInDate).date;
+    let monthName = months[d.getMonth()];
+  
+    
+
+    if (monthName) {
+
+    }
+
+
+   }
+   function EstimatebtnOnClick() {
+    console.log("EstimatebtnOnClick")
+
+   //Call HTML elements
+    
+    
+}
